a0bf5e11655ad13f26c8946e264490865933b8c5
张健 <2846325936@qq.com>
2016-02-27 20:56:02 +0800

    允许图书编号为1位，也允许用各国文字作图书编号

--- models/book.rb
+++ models/book.rb
@@ -10,10 +10,7 @@ class Book < Sequel::Model
     super
     validates_presence :code
     validates_unique :code
-    validates_min_length 3, :code
     validates_max_length 35, :code
-    # TODO: 图书编号应该允许汉字
-    validates_format /\A[a-z\-_A-Z0-9]+\z/, :code, message: I18n.t('book.code_valid_combine_format')
   end
 
   def info
